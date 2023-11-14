2a307b6e06bd175d25df1e1985c2592ddde0426d
alicanyucel <yucelalican@hotmail.com>
2023-11-14 17:24:39 +0300

    in model fiiled.

--- src/app/Models/product.model.ts
+++ src/app/Models/product.model.ts
@@ -1,1 +1,13 @@
-///
+export class Product{
+    id:number;
+    title:string;
+    description:string;
+    price:number;
+    discountPercentage:number;
+    rating:number;
+    stock:number;
+    brand:string;
+    category:string;
+    thumbnail:string;
+    images:string[];
+}
