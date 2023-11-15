c02552a726d752b713dcd1e878f4ac320609f647
alicanyucel <yucelalican@hotmail.com>
2023-11-15 10:43:03 +0300

    service fiiled.

---
+++ src/app/services/error.service.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorService {
+
+  constructor() { }
+errorHandler(err:HttpErrorResponse)
+{
+  if(err.status==400)
+  {
+    console.log("Api adresine ulaşılamıyor");
+  }
+  console.log(err);
+}
+  
+}
--- src/app/services/my.service.ts
+++ src/app/services/my.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment.prod";
+import { ErrorService } from "./error.service";
 @Injectable({
   providedIn: 'root'
 })
 export class MyService {
-
-  constructor() { }
+  constructor(
+    private _http: HttpClient,
+    private _error: ErrorService
+  ) { }
+  get(callback: (res: any) => void) {
+    return this._http.get(`${environment.apiUrl}`).subscribe({
+      next: (res) => {
+        callback(res);
+      },
+      error: (err: HttpErrorResponse) => {
+        this._error.errorHandler(err);
+      }
+    })
+  }
 }
